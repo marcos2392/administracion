6840d81399b9d547451f4e1d8ccbbb4d8e71fcc1
marcos2392 <marckos_23@live.com>
2017-06-20 15:07:43 -0600

    c

--- src/Controller/NominasController.php
+++ src/Controller/NominasController.php
@@ -193,6 +193,7 @@ class NominasController extends AppController
 
             foreach($info_empleado as $info): 
                 $pago_joyeria=0;
+                $comision=0;
                 $hrstotales=$this->horasoperacion($empleado["horas"]);
 
                 $info_checadas=$this->infochecada($inicio_nomina,$termino_nomina,$sucursal); 
